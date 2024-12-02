5cc1aacdd6ad0716be5cb58413a08921e421ad53
Leonardo Ramos <leonardo.ramos@sap.com>
2024-12-02 11:20:51 -0300

    second star

---
+++ day-1/similarityScore.js
@@ -0,0 +1,41 @@
+const fs = require('node:fs');
+
+const VALUE_SEPARATOR = '   ';
+
+function main() {
+    const listsPath = process.argv[2];
+
+    const fileData = fs.readFileSync(listsPath, 'utf8')
+    const firstList = [];
+    const secondListMap = new Map();
+    fileData.split('\n').forEach(addToParallelLists(firstList, secondListMap));
+
+    return firstList.map(calculateSimilarity(secondListMap)).reduce((a, b) => a + b, 0);
+}
+
+function calculateSimilarity(secondListMap) {
+    return first => {
+        const currentValue = secondListMap.get(first);
+        if(!currentValue) {
+            return 0;
+        } else {
+            return first * currentValue;
+        }
+    }
+}
+
+function addToParallelLists(firstList, secondListMap) { 
+    return line => {
+        const [first, second] = line.split(VALUE_SEPARATOR);
+        firstList.push(first);
+        const currentValue = secondListMap.get(second);
+        if(!currentValue) {
+            secondListMap.set(second, 1)
+        } else {
+            secondListMap.set(second, currentValue + 1);
+        }
+    }
+}
+
+
+console.log(main());
